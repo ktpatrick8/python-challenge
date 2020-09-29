4f20de2a3a2bfb43a1094ae50fa9feb8fbcf3dcf
Kyle Patrick <kylepatrick@Kyles-MBP-2.attlocal.net>
2020-09-29 18:43:01 -0400

    Update Submission

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,19 +10,47 @@ with open(election_csv,'r') as csvfile:
     csv_header = next(csvfile)
 
     total_votes = 0
+    candidates = []
+    vote_list = []
 
     for row in csv_reader:
         total_votes += 1
 
+        name = str(row[2])
+        # vote_list.append(name)
+        if name not in candidates:
+            candidates.append(name)
+
+        if name == candidates[0]:
+            vote_list[0] = vote_list[0] + 1
+
+
+    # def result_calc(vote_list):
+    #     vote_count = vote_list.count(value)
+    #     vote_percentage = vote_count / total_v
+
+    # khan_votes = vote_list.count("Khan")
+    # correy_votes = vote_list.count("Correy")
+    # li_votes = vote_list.count("Li")
+    # otooley_votes = vote_list.count("O'Tooley")
+
+    # khan_percent = (khan_votes / total_votes) * 100
+    # correy_percent = (correy_votes / total_votes) * 100
+    # li_percent = (li_votes / total_votes) * 100
+    # otooley_percent = (otooley_votes / total_votes) * 100
+
+print(candidates)
+print(vote_list)
+# print(khan_percent)
 
 
 
 
-#print out analysis
-print(f'Financial Analysis {total_votes}')
+# #print out analysis
+# print(f'Financial Analysis {total_votes}')
 
 
-results_path = os.path.join("results.txt")
-results = open(results_path, "w+")
-#print out analysis
-results.write(f'Financial Analysis' + "\n")
+# results_path = os.path.join("results.txt")
+# results = open(results_path, "w+")
+# #print out analysis
+# results.write(f'Financial Analysis' + "\n")
